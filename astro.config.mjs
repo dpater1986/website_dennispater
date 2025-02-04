f836cd812c21e79044037f5f076ea5d35ae2df66
dpater1986 <d.pater1986@gmail.com>
2025-02-04 22:52:09 +0100

    Fallow a tutorial. Go to island...

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://localhost:4321",
   vite: {
     plugins: [tailwindcss()]
   }
