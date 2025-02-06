1603b8c9a0b9df0ca474a0c0af7d174945ff406b
dpater1986 <d.pater1986@gmail.com>
2025-02-06 16:39:17 +0100

    End turtorial and get project from github as starting base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://localhost:4321",
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: "https://example.com",
+  integrations: [preact()],
 });
